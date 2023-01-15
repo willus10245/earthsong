5027f2d27f992ac1c538c0cb17d71443fdeec743
Scott <dscottwiggins@gmail.com>
2023-01-14 20:20:53 -0500

    change bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: [
     './src/**/*.njk'
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [require('@tailwindcss/typography'), require("daisyui")],
   daisyui: {themes: ["garden"]}
 }
